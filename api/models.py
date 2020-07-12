92dd8794f3e9cff9018f09bdfe47a728efb6d3b9
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-12 23:35:55 +0100

    enhancement: added docstrings to magic methods to Profile object

--- api/models.py
+++ api/models.py
@@ -9,8 +9,7 @@ class Profile(models.Model):
     phone = models.CharField( blank=True, unique=True, null=True, max_length=19)
     profile = models.ImageField(upload_to='profile/', blank=True, null=True)
 
-    """
-    String for representing the Profile object (in Admin site etc.).
-    """
+
     def __str__(self):
         return self.user.first_name
+        """String for representing the Profile object (in Admin site etc.)"""
