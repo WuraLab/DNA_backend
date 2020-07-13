018d3417e84de50c0caa35089f0a85fec43539a6
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-13 02:08:30 +0100

    enhancement:passed serializer.py pylint-django test

--- api/serializers.py
+++ api/serializers.py
@@ -1,20 +1,21 @@
-from rest_framework import serializers
-from .models import Profile
+"""Serializers converted to querys into JSON, XML or other content types."""
+
 from django.contrib.auth.models import User
+from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import obtain_auth_token
-
+from .models import Profile
 
 class UserRegistrationSerializers(serializers.ModelSerializer):
-
-    class Meta:
+    """User registration  api data formatter"""
+    class Meta:  # pylint: disable=too-few-public-methods  
+        """Return default User options fields."""
         model = User
         fields = ('id', 'first_name', 'last_name', 'username', 'email', 'password')
         extra_kwargs = {
             'password': {'write_only': True, 'required': True}
         }
-
-    def create(self, validated_data):
+    def create(self, validated_data): # create User && Profile profile model.
         profile_data = validated_data
         user = User.objects.create_user(**profile_data)
         Token.objects.create(user=user)
@@ -22,13 +23,17 @@ class UserRegistrationSerializers(serializers.ModelSerializer):
         return user
 
 class ProfileSerializer(serializers.ModelSerializer):
+    """User Profile  api data formatter."""
     user = UserRegistrationSerializers()
     token = obtain_auth_token
-    class Meta:
+    class Meta:  # pylint: disable=too-few-public-methods  
+        """Return Profile fields."""
         model = Profile
-        fields = ('id',  'facebook_user','phone', 'profile', 'user' )
+        fields = ('id', 'facebook_user', 'phone', 'profile', 'user',)
 
 class EditProfileSerilizer(serializers.ModelSerializer):
-    class Meta:
+    """Update/edit user profile api data formatter."""
+    class Meta:  # pylint: disable=too-few-public-methods  
+        """Return optional Profile fields."""
         model = Profile
-        fields = ('id', 'facebook_user', 'phone', 'profile',  )
+        fields = ('id', 'facebook_user', 'phone', 'profile',)
