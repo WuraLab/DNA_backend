4ad56627b6e97e26eb7ed80c18e04a16656c4593
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-13 03:32:39 +0100

    enhancement:Missing docstring in public nested class (D106) and  Trailing whitespace in serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -8,8 +8,7 @@ from .models import Profile
 
 class UserRegistrationSerializers(serializers.ModelSerializer):
     # User registration  api data formatter.
-     #pylint: disable=too-few-public-methods
-    class Meta: 
+    class Meta:    #pylint: disable=too-few-public-methods
         # Return default User options fields.
         model = User
         fields = ('id', 'first_name', 'last_name', 'username', 'email', 'password')
@@ -28,8 +27,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     # User Profile  api data formatter.
     user = UserRegistrationSerializers()
     token = obtain_auth_token
-
-    class Meta:  #pylint: disable=too-few-public-methods
+    class Meta:    #pylint: disable=too-few-public-methods
         # Return Profile fields.
         model = Profile
         fields = ('id', 'facebook_user', 'phone', 'profile', 'user',)
