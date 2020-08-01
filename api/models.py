f9f6450761615b500f2da3cd3ce6449ecc55b632
omidiora <omidioraemmanuel@gmail.com>
2020-08-01 18:12:25 +0100

    added the Loan_Record

--- api/models.py
+++ api/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.conf import settings
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-from django.db.models import F
-from django.core.signals import request_finished
 
 # from django.core.validators import MaxLengthValidator, MinLengthValidator
 
