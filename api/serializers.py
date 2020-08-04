75408e7f4c11012105f3f846af99cbd0e95477e4
omidiora <omidioraemmanuel@gmail.com>
2020-08-04 19:14:10 +0100

    fix codacy issues

--- api/serializers.py
+++ api/serializers.py
@@ -9,11 +9,10 @@ from .models import Profile,Loan_Record
 class UserRegistrationSerializers(serializers.ModelSerializer):
     # User registration  api data formatter.
     class Meta:
-
         """
         Return Meta class
         """
-        
+
         #pylint: disable=too-few-public-methods
         # Return default User options fields.
         model = User
