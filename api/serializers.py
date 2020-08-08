fa84d21a67b8b2d48121e156f4e8ad6e9be388e1
omidiora <omidioraemmanuel@gmail.com>
2020-08-08 12:46:25 +0100

    fixing 2

--- api/serializers.py
+++ api/serializers.py
@@ -1,20 +1,25 @@
+# Serializers converted to query's into JSON, XML or other content types.
+
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import obtain_auth_token
-from .models import Profile
+from .models import Profile,Loan_Record
 
 class UserRegistrationSerializers(serializers.ModelSerializer):
-    # User registration  api data formatter.
-    class Meta:  #pylint: disable=too-few-public-methods
-        # Return default User options fields.
+    
+    class Meta:
+
+        '''Return Meta class'''
+        #pylint: disable=too-few-public-methods
         model = User
         fields = ('id', 'first_name', 'last_name', 'username', 'email', 'password')
         extra_kwargs = {
             'password': {'write_only': True, 'required': True}
         }
     #pylint: disable=R0201
-    def create(self, validated_data): #create User && Profile profile model.
+    def create(self, validated_data):
+        #create User && Profile profile model.
         profile_data = validated_data
         user = User.objects.create_user(**profile_data)
         Token.objects.create(user=user)
@@ -22,17 +27,32 @@ class UserRegistrationSerializers(serializers.ModelSerializer):
         return user
 
 class ProfileSerializer(serializers.ModelSerializer):
-    # User Profile  api data formatter.
+    #User Profile  api data formatter.
     user = UserRegistrationSerializers()
     token = obtain_auth_token
-    class Meta:    #pylint: disable=too-few-public-methods
-        # Return Profile fields.
+
+    class Meta:
+        '''Return Meta class'''
+        #pylint: disable=too-few-public-methods
         model = Profile
         fields = ('id', 'facebook_user', 'phone', 'profile', 'user',)
 
 class EditProfileSerilizer(serializers.ModelSerializer):
-    #Update/edit user profile api data formatter.
-    class Meta:  #pylint: disable=too-few-public-methods
-        # Return optional Profile fields.
+
+    class Meta:
+        '''Return Meta class'''
+        #pylint: disable=too-few-public-methods
+        #Return optional Profile fields.
         model = Profile
         fields = ('id', 'facebook_user', 'phone', 'profile',)
+
+
+
+class AddLoanSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        '''Return Meta class'''
+        #pylint: disable=too-few-public-methods
+        #Return optional Profile fields.
+        model=Loan_Record
+        fields=('id','user','amount','interest_rate','description','balance_to_pay',"due_date")
