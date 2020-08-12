be707105b193759ae8cc435e3d5868eed3eae272
peterson oaikhenah <peterson.oaikhenah@glovestcapital.com>
2020-08-12 19:09:06 +0100

    updated with latest api's and settings

--- DNA/settings.py
+++ DNA/settings.py
@@ -107,12 +107,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'DNA.wsgi.application'
 # EMAIL_BACKEND = config('EMAIL_BACKEND')
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_PORT = config('EMAIL_PORT')
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+
+EMAIL_HOST_USER = 'debtapp95@gmail.com'
+EMAIL_HOST_PASSWORD = 'gfrawjggnnedcirz'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 
 
 # Database
