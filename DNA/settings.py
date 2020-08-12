e48565d0da6b61721fea57f9529fe67689795994
peterson oaikhenah <peterson.oaikhenah@glovestcapital.com>
2020-08-12 18:58:51 +0100

    updated with latest api's

--- DNA/settings.py
+++ DNA/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 SECRET_KEY = '_37@a5jgf3g)5+n4*5lg-0j8jr_sb7+w707u#0hy&o)oclh=jd'
-SECRET_KEY = config('SECRETKEY')
+# SECRET_KEY = config('SECRETKEY')
 DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = ['https://dnappserver.herokuapp.com/', '127.0.0.1']
