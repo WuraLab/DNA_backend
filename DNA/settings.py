75dfa00049f8f791efb0d09c40fd45d3315b9fa6
peterson oaikhenah <peterson.oaikhenah@glovestcapital.com>
2020-08-12 18:57:46 +0100

    updated secret  key

--- DNA/settings.py
+++ DNA/settings.py
@@ -19,7 +19,7 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# SECRET_KEY = '_37@a5jgf3g)5+n4*5lg-0j8jr_sb7+w707u#0hy&o)oclh=jd'
+SECRET_KEY = '_37@a5jgf3g)5+n4*5lg-0j8jr_sb7+w707u#0hy&o)oclh=jd'
 SECRET_KEY = config('SECRETKEY')
 DEBUG = config("DEBUG")
 
