eeb24efc8c225175a42cd953fe9424a61b62d433
nextwebb <peterson.oaikhenah@glovestcapital.com>
2020-10-05 23:41:15 +0100

    removed trailing space

--- DNA/settings.py
+++ DNA/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 'django.contrib.messages',
 'django.contrib.sites',
 'django.contrib.staticfiles',
+'django_inlinecss',
 'rest_framework',
 'rest_framework.authtoken',
 'rest_auth',
