040955f7cfee50b90a1fdffb3ab544848a2582dc
nextwebb <peterson.oaikhenah@glovestcapital.com>
2020-10-06 00:03:00 +0100

    removed trailing whitespace

--- api/views.py
+++ api/views.py
@@ -507,11 +507,9 @@ class PaymentViewSet(viewsets.ModelViewSet):
                     #print(request.data)
                     return Response(f'response : {response}', status=status.HTTP_200_OK)
                 except NameError:
-                    return Response(f'Some error occured, try again later: {NameError}', status=status.HTTP_400_BAD_REQUEST)     
+                    return Response(f'Some error occured, try again later: ', status=status.HTTP_400_BAD_REQUEST)     
             else:
                 return Response('It appears some paramenters are empty', status=status.HTTP_400_BAD_REQUEST)
         else:
             response = {'message': 'API version not identified!'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
-    
-
