b309a96600eae326655ac2706a8f02e97c0bed64
omidiora <omidioraemmanuel@gmail.com>
2020-10-06 11:49:28 +0100

    The Delete Features for loan and account

--- api/views.py
+++ api/views.py
@@ -19,7 +19,6 @@ from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from rest_auth.registration.views import SocialLoginView
-from django.http import Http404
 
 
 
