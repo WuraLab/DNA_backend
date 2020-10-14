57baf700b05a1a417b05868155a3f56ee6e7b8fd
nextwebb <peterson.oaikhenah@glovestcapital.com>
2020-10-14 06:16:57 +0100

    Created payment information

--- api/models.py
+++ api/models.py
@@ -1,21 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False, unique=True,
-                                related_name = 'user')
-    facebook_user = models.CharField(max_length=250, blank=True, null=True, unique=True )
-    phone = models.CharField( blank=True, unique=True, null=True, max_length=20)
-    profile = models.CharField(max_length=250, blank=True, null=True, unique=True )
-
+                                related_name='user')
+    facebook_user = models.CharField(max_length=250, blank=True, null=True, unique=True)
+    phone = models.CharField(blank=True, unique=True, null=True, max_length=20)
+    profile = models.CharField(max_length=250, blank=True, null=True, unique=True)
 
     def __str__(self):
         """one-line docstring for representing the Profile object."""
         return f'{self.user.first_name}'
-
-
 
 
 class Loan_Record(models.Model):
@@ -24,26 +22,28 @@ class Loan_Record(models.Model):
         ('loan given', 'Loan given'),
 
     )
-    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='record')
-    name=models.CharField(max_length=200)
-    loan_category=models.CharField(max_length=100, choices=loan_record)
-    due_date=models.DateTimeField()
-    created=models.DateTimeField(auto_now_add=True)
-    amount=models.IntegerField()
-    interest_rate=models.CharField(max_length=10, default=000)
-    paid=models.BooleanField(default=False)
-    lender=models.BooleanField(default=True)
-    description=models.TextField()
-    balance_to_pay=models.DecimalField(max_digits=15, decimal_places=2 , blank=True,null=True)
-
-
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='record')
+    name = models.CharField(max_length=200)
+    loan_category = models.CharField(max_length=100, choices=loan_record)
+    due_date = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    amount = models.IntegerField()
+    interest_rate = models.CharField(max_length=10, default=000)
+    paid = models.BooleanField(default=False)
+    lender = models.BooleanField(default=True)
+    description = models.TextField()
+    balance_to_pay = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
-       """one-line docstring for representing the L object."""
-       return self.description
+        """one-line docstring for representing the L object."""
+        return self.description
+
 
 class Payment(models.Model):
     loan = models.ForeignKey(Loan_Record, on_delete=models.CASCADE, related_name='payment_history')
-    created=models.DateTimeField(auto_now_add=True)
-    paid_amount=models.IntegerField()
+    paid_date = models.DateTimeField(default=False)
+    amount_paid = models.IntegerField(default=False)
+
+    def __str__(self):
+        """one-line docstring for representing the L object."""
+        return f'{self.loan.name} '
--- api/serializers.py
+++ api/serializers.py
@@ -3,66 +3,74 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import obtain_auth_token
-from .models import Profile ,Loan_Record, Payment
+from .models import Profile, Loan_Record, Payment
+
 
 class UserRegistrationSerializers(serializers.ModelSerializer):
     # User registration  api data formatter.
-    class Meta:  #pylint: disable=too-few-public-methods
+    class Meta:  # pylint: disable=too-few-public-methods
         # Return default User options fields.
         model = User
         fields = ('id', 'first_name', 'last_name', 'username', 'email', 'password')
         extra_kwargs = {
             'password': {'write_only': True, 'required': True}
         }
-    #pylint: disable=R0201
-    def create(self, validated_data): #create User && Profile profile model.
+
+    # pylint: disable=R0201
+    def create(self, validated_data):  # create User && Profile profile model.
         profile_data = validated_data
         user = User.objects.create_user(**profile_data)
         Token.objects.create(user=user)
         Profile.objects.create(user=user)
         return user
 
+
 class ProfileSerializer(serializers.ModelSerializer):
     # User Profile  api data formatter.
     user = UserRegistrationSerializers()
     token = obtain_auth_token
-    class Meta:    #pylint: disable=too-few-public-methods
+
+    class Meta:  # pylint: disable=too-few-public-methods
         # Return Profile fields.
         model = Profile
         fields = ('id', 'facebook_user', 'phone', 'profile', 'user',)
 
+
 class EditProfileSerilizer(serializers.ModelSerializer):
-    #Update/edit user profile api data formatter.
-    class Meta:  #pylint: disable=too-few-public-methods
+    # Update/edit user profile api data formatter.
+    class Meta:  # pylint: disable=too-few-public-methods
         # Return optional Profile fields.
         model = Profile
         fields = ('id', 'facebook_user', 'phone', 'profile',)
 
+
 class LoanSerializer(serializers.ModelSerializer):
-    #AddLoan  api data formaterr.
-    class Meta:   #pylint: disable=too-few-public-methods
-        #Return optional model loan record
-        model=Loan_Record
-        fields=('id','user','name', 'amount','interest_rate','description','balance_to_pay',"due_date","created")
+    # AddLoan  api data formaterr.
+    class Meta:  # pylint: disable=too-few-public-methods
+        # Return optional model loan record
+        model = Loan_Record
+        fields = (
+        'id', 'user', 'name', 'amount', 'interest_rate', 'description', 'balance_to_pay', "due_date", "created")
 
 
-class  EditLoanProfileSerilizer(serializers.ModelSerializer):
-    #AddLoan  api data formaterr.
-    class Meta:   #pylint: disable=too-few-public-methods
-        #Return optional model loan record
-        model=Loan_Record
-        fields=('id', 'name', 'amount','interest_rate','description','balance_to_pay',"due_date")
+class EditLoanProfileSerilizer(serializers.ModelSerializer):
+    # AddLoan  api data formaterr.
+    class Meta:  # pylint: disable=too-few-public-methods
+        # Return optional model loan record
+        model = Loan_Record
+        fields = ('id', 'name', 'amount', 'interest_rate', 'description', 'balance_to_pay', "due_date")
 
 
 class DeleteAccountSerializer(serializers.ModelSerializer):
-        #AddLoan  api data formaterr.
-        class Meta:  #pylint: disable=too-few-public-methods
-            #Return optional model loan record
-            model=User
-            fields= '__all__'
+    # AddLoan  api data formaterr.
+    class Meta:  # pylint: disable=too-few-public-methods
+        # Return optional model loan record
+        model = User
+        fields = '__all__'
 
-class  PaymentSerializer(serializers.ModelSerializer):
-    #create api data formaterr.
-    class Meta: #pylint: disable=too-few-public-methods
-        model=Payment
-        fields=('id', 'loan', 'created',"paid_amount")
+
+class PaymentSerializer(serializers.ModelSerializer):
+    # create api data formaterr.
+    class Meta:  # pylint: disable=too-few-public-methods
+        model = Payment
+        fields = ('id', 'loan', 'paid_date', "amount_paid")
