c0e904cb880c2e14b100bd548ef1d7289aff7f08
nextwebb <oaikhenahpeterson@gmail.com>
2020-12-10 23:59:38 +0100

    Added loan category choices

--- api/models.py
+++ api/models.py
@@ -5,8 +5,7 @@ from django.contrib.auth.models import User
 # from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False, unique=True,
-                                related_name='user')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False, unique=True, related_name='user')
     facebook_user = models.CharField(max_length=250, blank=True, null=True, unique=True)
     phone = models.CharField(blank=True, unique=True, null=True, max_length=20)
     profile = models.CharField(max_length=250, blank=True, null=True, unique=True)
@@ -17,20 +16,18 @@ class Profile(models.Model):
 
 
 class Loan_Record(models.Model):
-    loan_record = (
-        ('loan taken', 'loan taken'),
-        ('loan given', 'Loan given'),
-
+    LOAN_CATEGORY_CHOICES = (
+        ('loan_taken', 'loan taken'),
+        ('loan_given', 'Loan given'),
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='record')
     name = models.CharField(max_length=200)
-    loan_category = models.CharField(max_length=100, choices=loan_record)
+    loan_category = models.CharField(max_length=10, choices=LOAN_CATEGORY_CHOICES)
     due_date = models.DateTimeField()
     created = models.DateTimeField(auto_now_add=True)
     amount = models.IntegerField()
     interest_rate = models.CharField(max_length=10, default=000)
     paid = models.BooleanField(default=False)
-    lender = models.BooleanField(default=True)
     description = models.TextField()
     balance_to_pay = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
     completed = models.BooleanField(default=False)
